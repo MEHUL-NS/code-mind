e1cb5a07debd5b16132b95cea2a098239b139b51
Mehul Sutrave <sutravenmehul@gmail.com>
2023-07-15 12:16:56 +0530

    added component to display data from server

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,25 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <script lang="ts">
-    import axios from "axios"
     import { onMount } from "svelte";
-    const fetchData  = async() => {
-        const res = await axios.get("https://projecteuler.net/problem=1");
-        console.log(res)
-    };
-
-    onMount(() => {
-        fetchData();
+    import Tester from "../Components/Tester.svelte";
+ 
+    onMount(() => {       
         console.log("Hello")
     } )
 
 </script>
+
+<a href="/src/routes/testing.svelte">Click me</a>
+
+
+<div class="giveSpacing"></div>
+<style>
+    .giveSpacing{
+        height: 200px;
+    }
+</style>
+
+<Tester />
 
 
