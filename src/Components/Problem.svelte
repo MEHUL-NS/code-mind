a050c3a8c1fff0e7cad46a06940b0c73f315657a
Mehul Sutrave <sutravenmehul@gmail.com>
2023-07-15 12:46:17 +0530

    added problem statement component

---
+++ src/Components/Problem.svelte
@@ -0,0 +1,40 @@
+<h1>
+    This is from a component
+</h1>
+
+
+<script lang="ts">
+    import axios from "axios";
+    import { onMount } from "svelte";
+
+    const probset = {
+        name : "",
+        nos : 0,
+        statement : ""        
+    }
+    
+
+    const fetchData = async() => {
+        const res = await axios.get(`http://localhost:5000/displayProblem`)
+        probset.name = res.data.pName;
+        probset.nos = res.data.pNo;
+        probset.statement = res.data.pStatement
+       
+
+    }
+    
+    onMount(() => {
+        fetchData();
+    })
+</script>
+
+
+<div>
+    <p>Name : {probset.name}</p>
+
+    <p>Number : {probset.nos}</p>
+
+    <p>Name : {probset.statement}</p>
+
+</div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <script lang="ts">
     import { onMount } from "svelte";
-    import Tester from "../Components/Tester.svelte";
+    import Problem from "../Components/Problem.svelte";
  
     onMount(() => {       
         console.log("Hello")
@@ -11,16 +8,8 @@
 
 </script>
 
-<a href="/src/routes/testing.svelte">Click me</a>
 
 
-<div class="giveSpacing"></div>
-<style>
-    .giveSpacing{
-        height: 200px;
-    }
-</style>
 
-<Tester />
-
+<Problem />
 
